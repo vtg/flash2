5e23df9597d650117115d1d9b8803ecb7c28ff1b
Victor Yunevich <v.t.g.m.b.x@gmail.com>
2015-02-23 01:42:20 +0300

    mode model into subpackage

--- model/model_test.go
+++ model/model_test.go
@@ -1,9 +1,22 @@
-package rapi
+package model
 
-import "testing"
+import (
+	"runtime"
+	"testing"
+)
 
 type M struct {
 	Model
+}
+
+func assertEqual(t *testing.T, expect interface{}, v interface{}) {
+	if v != expect {
+		_, fname, lineno, ok := runtime.Caller(1)
+		if !ok {
+			fname, lineno = "<UNKNOWN>", -1
+		}
+		t.Errorf("FAIL: %s:%d\nExpected: %#v\nReceived: %#v", fname, lineno, expect, v)
+	}
 }
 
 var m = M{}

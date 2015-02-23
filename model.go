90cb77c76f125c990d37a912973b729034c0ccdf
Victor Yunevich <v.t.g.m.b.x@gmail.com>
2015-02-23 04:00:49 +0300

    add model to package

--- model.go
+++ model.go
@@ -1,4 +1,4 @@
-package model
+package flash
 
 import (
 	"fmt"
--- model_test.go
+++ model_test.go
@@ -1,22 +1,9 @@
-package model
+package flash
 
-import (
-	"runtime"
-	"testing"
-)
+import "testing"
 
 type M struct {
 	Model
-}
-
-func assertEqual(t *testing.T, expect interface{}, v interface{}) {
-	if v != expect {
-		_, fname, lineno, ok := runtime.Caller(1)
-		if !ok {
-			fname, lineno = "<UNKNOWN>", -1
-		}
-		t.Errorf("FAIL: %s:%d\nExpected: %#v\nReceived: %#v", fname, lineno, expect, v)
-	}
 }
 
 var m = M{}
